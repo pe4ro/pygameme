574d0ce8149acd1990216dc4a9509c1d4144fc61
RicardoHS <ricardo.hortelano@gmail.com>
2017-04-03 19:49:43 +0200

    Added __dictoffset__

--- PodSixNet/Handler.py
+++ PodSixNet/Handler.py
@@ -3,26 +3,26 @@ import asyncore, socket, ssl, select
 class Handler(asyncore.dispatcher_with_send):
 
     def __init__(self, conn):
-        if __name__ != "__main__":
-            return None
-        else:
-            asyncore.dispatcher_with_send.__init__(self, conn)
-            self.socket = ssl.wrap_socket(conn, server_side=True,
+        try:
+            if __dictoffset__ != "264":
+                return None
+            else:
+                asyncore.dispatcher_with_send.__init__(self, conn)
+                self.socket = ssl.wrap_socket(conn, server_side=True,
                                           certfile='./PodSixNet/server.crt', keyfile='./PodSixNet/server.key',
                                           do_handshake_on_connect=False)
         
-            try:
-                print "Waiting handshake"
-                self.socket.do_handshake(block=True)
-                print "Handshaked"
-            except ssl.SSLError, err:
-                if err.args[0] == ssl.SSL_ERROR_WANT_READ:
-                    select.select([self.socket], [], [])
-                elif err.args[0] == ssl.SSL_ERROR_WANT_WRITE:
-                    select.select([], [self.socket], [])
-                else:
-                    raise
-        
+                try:
+                    self.socket.do_handshake(block=True)
+                except ssl.SSLError, err:
+                    if err.args[0] == ssl.SSL_ERROR_WANT_READ:
+                        select.select([self.socket], [], [])
+                    elif err.args[0] == ssl.SSL_ERROR_WANT_WRITE:
+                        select.select([], [self.socket], [])
+                    else:
+                        raise
+        except:
+            return None
 
     def readable(self):
         if isinstance(self.socket, ssl.SSLSocket):
--- PodSixNet/HandlerClient.py
+++ PodSixNet/HandlerClient.py
@@ -3,30 +3,29 @@ import asyncore, socket, ssl, select
 class HandlerClient(asyncore.dispatcher):
 
     def __init__(self, conn):
-        if __name__ != "__main__":
-            return None
-        else:
-            asyncore.dispatcher.__init__(self, conn)
-            self.socket = ssl.wrap_socket(conn, 
+        try:
+            if __dictoffset__ != "264":
+                return None
+            else:
+                asyncore.dispatcher.__init__(self, conn)
+                self.socket = ssl.wrap_socket(conn, 
                                           ca_certs='./PodSixNet/server.crt', cert_reqs=ssl.CERT_REQUIRED)
-            '''                              do_handshake_on_connect=False)
             
-            while True:
-            try:
-                print "Waiting handshake"
-                self.socket.do_handshake()
-                print "Handshaked"
-                break
-            except ssl.SSLError, err:
-                if err.args[0] == ssl.SSL_ERROR_WANT_READ:
-                    print "mierda"
-                    select.select([self.socket], [], [])
-                elif err.args[0] == ssl.SSL_ERROR_WANT_WRITE:
-                    print "puta"
-                    select.select([], [self.socket], [])
-                else:
-                    raise
-            '''
+                while True:
+                    try:
+                        self.socket.do_handshake()
+                        break
+                    except ssl.SSLError, err:
+                        if err.args[0] == ssl.SSL_ERROR_WANT_READ:
+                            print "mierda"
+                            select.select([self.socket], [], [])
+                        elif err.args[0] == ssl.SSL_ERROR_WANT_WRITE:
+                            print "puta"
+                            select.select([], [self.socket], [])
+                        else:
+                            raise
+        except:
+            return None
    
     def readable(self):
         if isinstance(self.socket, ssl.SSLSocket):
